2bf27c75189f226f58b34243890bfb7ee3b19d89
Djamel BENDAOUD <djamel.bendaoud@qonto.com>
2021-10-21 14:36:00 +0200

    Add embroider safe & optimized test

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function() {
   return {
     useYarn: true,
     scenarios: [
+      embroiderSafe({ allowedToFail: true }),
+      embroiderOptimized({ allowedToFail: true }),
       {
         name: 'ember-lts-3.12',
         npm: {
@@ -38,27 +41,10 @@ module.exports = async function() {
           }
         }
       },
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
       {
         name: 'ember-default',
         npm: {
           devDependencies: {}
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^0.5.1'
-          }
         }
       }
     ]
